7c91076578322541127d96a51f17fb06bb87b1ae
Katherine Lewis <kale8432@colorado.edu>
2019-07-31 10:30:34 -0600

    fetching animals

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
-import {getAnimals, getDonations} from '../../actions';gi
+import {getAnimals, getDonations, hasError} from '../../actions';
+import {fetchData} from '../../util/apicalls';
 
 export class App extends Component {
+
+  async componentDidMount(){
+    await this.fetchAnimals()
+  }
+
+  fetchAnimals = async () => {
+    if(!this.props.animals.length){
+      try{
+        const url = 'http://localhost:3001/api/v1/rescue-animals';
+        const results = await fetchData(url);
+        console.log(resutls)
+        this.props.getAnimals(results)
+      } catch(error){
+        this.props.hasError(error.message)
+      }
+    }
+  }
+
 
   render() {
     // const animal = this.props.animals.map(animals => {
@@ -12,7 +31,8 @@ export class App extends Component {
     //       <p></p>
     //     </article>
     //   )
-    // })
+    // }
+
     return (
       <div>
         
@@ -28,7 +48,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = dispatch => ({
   getAnimals: animals => dispatch(getAnimals(animals)),
-  getDonations: donations => dispatch(getDonations(donations))
+  getDonations: donations => dispatch(getDonations(donations)),
+  hasError: error => dispatch(hasError(error))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
