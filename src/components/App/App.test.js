769fe5a75900f0428075b5b523836f5d217c7b34
Katherine Lewis <kale8432@colorado.edu>
2019-07-31 10:45:53 -0600

    writing tests for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import App from './App';
 import {shallow} from 'enzyme';
+import {mockAnimals, mockDonations, mockError, mockLoading} from '../../util/mockData';
+import * as action from '../../actions';
 
 describe('App', () => {
   let wrapper;
@@ -12,10 +14,42 @@ describe('App', () => {
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
-  it('should map state to props', () => {
 
+  describe('MSTP', () => {
+    it('should map state to props', () => {
+      const mockState = {
+        animals: mockAnimals,
+        donations: mockDonations
+      }
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(mockState)
+    });
   });
-  it('should map dispatch to props', () => {
 
+  describe('MDTP', () => {
+    it('should call getAnimals when getAnimals action is dispatched', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = action.getAnimals(mockAnimals);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      expect(mappedProps).toHaveBeenCalledWith(actionToDispatch);
+    });
+    it('should call getDonations when getDonations action is dispatched', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = action.getDonation(mockDonation);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      expect(mappedProps).toHaveBeenCalledWith(actionToDispatch);
+    });
+    it('should call hasError when hasError action is dispatched', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = action.hasError(mockError);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      expect(mappedProps).toHaveBeenCalledWith(actionToDispatch);
+    });
+    it('should call isLoading when isLoading action is dispatched', () => {
+      const mockDispatch = jest.fn();
+      const actionToDispatch = action.isLoading(mockLoading);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      expect(mappedProps).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
-})
+  });
