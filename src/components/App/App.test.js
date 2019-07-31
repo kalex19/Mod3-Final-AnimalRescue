1a1f0904c1c31d7c150c7915a0aa8b14c9fd644a
Katherine Lewis <kale8432@colorado.edu>
2019-07-31 10:25:47 -0600

    buidling fetch call for animals in app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should map state to props', () => {
+
+  });
+  it('should map dispatch to props', () => {
+
+  });
+})
